58450d6752ef50b310f5f783c8642d22dca6392c
Samantha Guergenenov <samguergen@gmail.com>
2018-02-02 16:34:38 -0500

    replace path to all images

--- app/app/app.js
+++ app/app/app.js
@@ -12,92 +12,92 @@ myApp.config(function($stateProvider, $urlRouterProvider, $locationProvider){
       })
       .state('become-member', {
         url: '/become-member',
-        templateUrl: '../views/become-member.html',
+        templateUrl: 'views/become-member.html',
         // controller: 'MainController'
       })
       .state('community', {
         url: '/community',
-        templateUrl: '../views/community.html',
+        templateUrl: 'views/community.html',
         // controller: 'MainController'
       })
       .state('contact', {
         url: '/contact',
-        templateUrl: '../views/contact.html',
+        templateUrl: 'views/contact.html',
         // controller: 'MainController'
       })
       .state('donate', {
         url: '/donate',
-        templateUrl: '../views/donate.html',
+        templateUrl: 'views/donate.html',
         // controller: 'MainController'
       })
       .state('faces', {
         url: '/faces-of-our-members',
-        templateUrl: '../views/faces.html',
+        templateUrl: 'views/faces.html',
         // controller: 'MainController'
       })
       .state('family', {
         url: '/family-involvement',
-        templateUrl: '../views/family.html',
+        templateUrl: 'views/family.html',
         // controller: 'MainController'
       })
       .state('faq', {
         url: '/faq',
-        templateUrl: '../views/faq.html',
+        templateUrl: 'views/faq.html',
         // controller: 'MainController'
       })
       .state('member-app', {
         url: '/member-application',
-        templateUrl: '../views/member-app.html',
+        templateUrl: 'views/member-app.html',
         // controller: 'MainController'
       })
       .state('member-programs', {
         url: '/member-programs',
-        templateUrl: '../views/member-programs.html',
+        templateUrl: 'views/member-programs.html',
         // controller: 'MainController'
       })
       .state('news', {
         url: '/news',
-        templateUrl: '../views/news.html',
+        templateUrl: 'views/news.html',
         // controller: 'MainController'
       })
       .state('non-rider-member', {
         url: '/non-rider-member',
-        templateUrl: '../views/non-rider-member.html',
+        templateUrl: 'views/non-rider-member.html',
         // controller: 'MainController'
       })
       .state('organization', {
         url: '/organization',
-        templateUrl: '../views/organization.html',
+        templateUrl: 'views/organization.html',
         // controller: 'MainController'
       })
       .state('pay-online', {
         url: '/pay-online',
-        templateUrl: '../views/pay-online.html',
+        templateUrl: 'views/pay-online.html',
         // controller: 'MainController'
       })
       .state('rider-stories', {
         url: '/rider-stories',
-        templateUrl: '../views/rider-stories.html',
+        templateUrl: 'views/rider-stories.html',
         // controller: 'MainController'
       })
       .state('what-we-do', {
         url: '/what-we-do',
-        templateUrl: '../views/what-we-do.html',
+        templateUrl: 'views/what-we-do.html',
         // controller: 'MainController'
       })
       .state('corporate', {
         url: '/corporate',
-        templateUrl: '../views/corporate.html',
+        templateUrl: 'views/corporate.html',
         // controller: 'MainController'
       })
       .state('volunteer-to-drive', {
         url: '/volunteer-to-drive',
-        templateUrl: '../views/volunteer-to-drive.html',
+        templateUrl: 'views/volunteer-to-drive.html',
         // controller: 'MainController'
       })
       .state('add-pta-credit', {
         url: '/add-pta-credit',
-        templateUrl: '../views/add-pta-credit.html',
+        templateUrl: 'views/add-pta-credit.html',
         // controller: 'MainController'
       })
 
