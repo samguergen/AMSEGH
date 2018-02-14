9f441f55c3e4c2c57a8e832b55612e25bece7937
Samantha Guergenenov <samguergen@gmail.com>
2018-02-13 19:26:33 -0500

    part1

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/app'));
+app.post('/sendmail', function(req, res){
+  console.log('post req', req);
+  res.end();
+});
+
+
+
+
+
+
+
 app.listen(process.env.PORT || 3000);
+
+
+
