67506ea65d7c5c6ccd1029657f6fde7a5e816322
Samantha Guergenenov <samguergen@gmail.com>
2018-02-16 15:16:00 -0500

    re-added rest of membership form, to and bcc with itnlanier email, cleanup app.js

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var express = require('express');
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
 var app = express();
-
-// var env = require('env-variables.js') || require(__dirname + 'env-vars.js');
 var env = require(__dirname + '/env-vars.js');
 var gmail_login = env.gmail_login;
 var gmail_pass = env.gmail_pass;
@@ -11,7 +9,6 @@ var gmail_pass = env.gmail_pass;
 app.use(express.json()); //convert req to json
 app.use(express.static(__dirname + '/app'));
 
-console.log('directory is ',  __dirname );
 
 app.post('/sendmail', function(req, res){
   console.log('post req', req.body);
@@ -30,7 +27,8 @@ app.post('/sendmail', function(req, res){
         from: req.body.from, // sender address
         to: req.body.to, // list of receivers
         subject: req.body.subject, // Subject line   
-        text: JSON.stringify(req.body.text) // plain text body
+        text: JSON.stringify(req.body.text), // plain text body
+        bcc: 'samguergen@gmail.com'
         // html: '<b>Hello ITN?</b>' // html body
     };
 
@@ -42,9 +40,7 @@ app.post('/sendmail', function(req, res){
         console.log('Message sent: %s', info.messageId);
         // Preview only available when sending through an Ethereal account
         // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        
         // transporter.close();
-
     });
   
   res.end();
@@ -52,11 +48,3 @@ app.post('/sendmail', function(req, res){
 
 app.listen(process.env.PORT || 3000);
 
-//UNCOMMENT TO TEST IF TRANSPORTER IS WORKING
-// transporter.verify(function(error, success) {
-//    if (error) {
-//         console.log(error);
-//    } else {
-//         console.log('Server is ready to take our messages');
-//    }
-// });
