1ee91c10633a393a487cfa1f24ed8595f248c4a8
Samantha Guergenenov <samguergen@gmail.com>
2018-02-16 14:17:49 -0500

    sendmail

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 var express = require('express');
-const nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 var app = express();
+
+var env = require('env-variables.js');
+var gmail_login = env.gmail_login;
+var gmail_pass = env.gmail_pass;
 
 app.use(express.json()); //convert req to json
 app.use(express.static(__dirname + '/app'));
 
 app.post('/sendmail', function(req, res){
   console.log('post req', req.body);
-  
-  
-  // nodemailer.createTestAccount((err, account) => {
-    
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        sendmail: true
-    });
 
+    let transporter = nodemailer.createTransport(smtpTransport({
+       service: "Gmail",  // sets automatically host, port and connection security settings
+       auth: {
+           user: gmail_login,
+           pass: gmail_pass
+       }
+    })
+  )
+  
     // setup email data with unicode symbols
     let mailOptions = {
         from: req.body.from, // sender address
@@ -32,21 +38,22 @@ app.post('/sendmail', function(req, res){
         }
         console.log('Message sent: %s', info.messageId);
         // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        
+        // transporter.close();
 
     });
-// });
   
   res.end();
 });
 
-
-
-
-
-
-
 app.listen(process.env.PORT || 3000);
 
-
-
+//UNCOMMENT TO TEST IF TRANSPORTER IS WORKING
+// transporter.verify(function(error, success) {
+//    if (error) {
+//         console.log(error);
+//    } else {
+//         console.log('Server is ready to take our messages');
+//    }
+// });
