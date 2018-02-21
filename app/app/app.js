29fc204495c7c8e114723f055ca808cc70bb638a
Samantha Guergenenov <samguergen@gmail.com>
2018-02-21 14:37:13 -0500

    state corrected

--- app/app/app.js
+++ app/app/app.js
@@ -132,6 +132,8 @@ myApp.controller('MainController', ['$scope', '$transitions','$http', function (
   };
   $scope.dataPDF = null;
   $scope.formSubject = 'New application received';
+  $scope.states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+
   
   // $transitions.onStart({}, function($transition, $scope){
   //     console.log('changing state');
