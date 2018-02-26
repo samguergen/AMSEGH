bc3d1a35604801c0f6a7710689a6d53b826fd92e
Samantha Guergenenov <samguergen@gmail.com>
2018-02-26 17:00:10 -0500

    minor comments

--- app/app/app.js
+++ app/app/app.js
@@ -94,6 +94,10 @@ myApp.config(function($stateProvider, $urlRouterProvider, $locationProvider){
         url: '/add-pta-credit',
         templateUrl: 'views/add-pta-credit.html'
       })
+      .state('draft', {
+        url: '/draft',
+        templateUrl: 'views/draft.html'
+      })
 
   // default fall back route
   $urlRouterProvider.otherwise('/');
