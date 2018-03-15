37fd2f8e1bd1f34270e667cca206575536705cbc
Samantha Guergenenov <samguergen@gmail.com>
2018-03-15 11:42:38 -0400

    google analytics implementation attempt try 1 - add tag to index.html and config in app.run and app.controller

--- app/app/app.js
+++ app/app/app.js
@@ -115,6 +115,23 @@ myApp.config(function($stateProvider, $urlRouterProvider, $locationProvider){
 })
 
 
+
+  myApp.run(['$rootScope', '$location', '$window',
+    function($rootScope, $location, $window) {
+      $rootScope.$on('$routeChangeSuccess',
+        function(event) {
+          if (!$window.ga) {
+            return;
+          }
+          $window.ga('send', 'pageview', {
+            page: $location.path()
+          });
+        });
+    }
+  ]);
+
+
+
 myApp.controller('MainController', ['$scope', '$transitions','$http', '$anchorScroll', '$location', '$stateParams', function ($scope, $transitions, $http, $anchorScroll, $location, $stateParams)  {
   console.log('inside main controller');
   $scope.zoomLevel = 1;
