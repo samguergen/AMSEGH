5b7bdcfb08bd16b2228f3a51897de9bdbb511670
Samantha Guergenenov <samguergen@gmail.com>
2018-03-20 17:18:12 -0400

    keyword search loads but only when relevant page

--- app/app/app.js
+++ app/app/app.js
@@ -168,6 +168,7 @@ myApp.controller('MainController', ['$scope', '$transitions','$http', '$anchorSc
   $scope.states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
   $scope.itnSources = ['Family','Friend','Speaker','Doctor','Radio','Television','Flier','Book','Phone','Agency on Aging', 'Social Worker','Internet','Referred by Current Member'];
   $scope.ratings = ['None',1,2,3,4,5,6];
+  $scope.keyword = '';
 
   
   // $transitions.onStart({}, function($transition, $scope){
@@ -230,11 +231,10 @@ myApp.controller('MainController', ['$scope', '$transitions','$http', '$anchorSc
     
   }
   
-  $scope.searchKeyword = function(keyword){
+  $scope.searchKeyword = function(){
     console.log("inside searchKeyword");
     var myHilitor = new Hilitor("wrapper-content");
-    console.log('my hilitor is ', myHilitor);
-    myHilitor.apply(keyword);
+    myHilitor.apply($scope.keyword);
   }
 
 
